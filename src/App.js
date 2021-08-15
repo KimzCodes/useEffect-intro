9d3daed9bb92fef77a241f71efd17c64cbb3b744
KimzCodes <81085401+KimzCodes@users.noreply.github.com>
2021-08-15 22:04:31 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -4,54 +4,54 @@ import "./styles.css";
 export default function App() {
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
+  //1
+  // rules for all useEffect statues:
+  //render
+  //useEffect
 
-  //effect with empty dependency array
-  //run after first render only
-  // useEffect(() => {
-  //   const fetchData = () => {
-  //     console.log("fetch data");
-  //   };
-  //   fetchData();
+  //useEffect with empty dependency array
+  //run one time only
+  //when: after first render
   // }, []);
 
-  //effect with dependency array
-  //run after first render
-  //run if dependency has been changed
   // useEffect(() => {
-  //   const fetchData = () => {
-  //     console.log("fetch data");
-  //   };
-  //   fetchData();
-  // }, [name]);
+  //   //ADD UR CODE
+  //   console.log("use effect one time");
+  // }, []); //useeffect dependency array
 
-  //effect without dependency array
-  //after first render
-  //run if any change happen
+  // ------------------------------------- //
+  //2
+  //useEffect with not empty dependency array
+  //run when update happen
+  //when: after first render, and when dependency updated
+
+  //render
+  //useEffect
+  //update the state -> re-render (sec render)
+  //UseEffect -> watcher -> dependency -> name ? updated -> yes -> do the effect / no -> skip the effect
+
   // useEffect(() => {
-  //   const fetchData = () => {
-  //     console.log("fetch data");
-  //   };
-  //   fetchData();
-  // });
-
-  //UseEffect with clean up
-  //first render
+  //   //ADD UR CODE
+  //   if (name || phone) {
+  //     console.log("update");
+  //   }
+  // }, [name, phone]); //useeffect dependency array
+  // //watcher -> state / props / var
   //use effect -> return clean / init clean
 
-  //change happen
-  //sec render
-  //excute clean up
-  //use effect
+  // ------------------------------------- //
+  //3
+  //useEffect with no dependency array
+  //run when update happen
+  //when: run after first render, and after re-render
+  // useEffect(() => {
+  //   console.log("effect");
+  // });
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      console.log("timeout");
-    }, 1000);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [name]);
+  //render
+  //change->state
+  //re render
+  //use Effect
 
   return (
     <div className="App">
